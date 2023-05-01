4b8ef44c0accf27583d474ddb4f61539b2c7d734
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-02 00:59:29 +0600

    Database

---
+++ MVC_11/Work_01/VIewModel/ActorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Work_01.Models.Metadata;
+
+namespace Work_01.VIewModel
+{
+    public class ActorViewModel
+    {
+        public int ActorId { get; set; }
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+        [Required, EnumDataType(typeof(Gender))]
+        public int Gender { get; set; }
+        [StringLength(150)]
+        public HttpPostedFileBase Picture { get; set; }
+        [Required]
+        public int MovieId { get; set; }
+    }
+}
