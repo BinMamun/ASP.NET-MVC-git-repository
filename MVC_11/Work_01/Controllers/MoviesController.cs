a64b0325520c75dad60df09efc09d8247086ee4a
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-03 00:35:41 +0600

    Movies_table

---
+++ MVC_11/Work_01/Controllers/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work_01.Models;
+using Work_01.Models.Metadata;
+using PagedList;
+
+namespace Work_01.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieDbContext db = new MovieDbContext();
+        // GET: Movies
+        public ActionResult Index(int pg=1)
+        {
+            ViewBag.Count = db.Movies.Count();            
+            return View(db.Movies.ToList().OrderBy(x=> x.MovieId).ToPagedList(pg, 5));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Movies m)
+        {
+            if (ModelState.IsValid)
+            {              
+                db.Movies.Add(m);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(m);
+        }
+
+        public ActionResult Edit(int id)
+        {            
+            return View(db.Movies.First(x => x.MovieId == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Movies m)
+        {
+            if (ModelState.IsValid)
+            {                
+                db.Entry(m).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(m);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.Movies.First(x => x.MovieId == id));
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            Movies m = db.Movies.First(x => x.MovieId == id);
+            db.Entry(m).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");           
+        }
+    }
+}
---
+++ MVC_11/Work_01/Models/Metadata/MoviesMetaData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Work_01.Models.Metadata
+{
+    public class MoviesType
+    {
+        public int MovieId { get; set; }
+        [Required, StringLength(50)]
+        public string Title { get; set; }
+        [Required, DataType(DataType.Date), Display(Name ="Release Date"), DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}")]
+        public System.DateTime ReleaseDate { get; set; }
+        [Required, StringLength(50), Display(Name ="IMDB Rating")]
+        public string ImdbRating { get; set; }
+        [Required, Display(Name ="Rotten Tomato Freshness")]
+        public int RottenTomatoFreshness { get; set; }
+    }
+}
