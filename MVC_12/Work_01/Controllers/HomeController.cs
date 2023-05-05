b43fbb8611b71e5740bed4cf0dd7763fdf48ab05
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-06 01:11:28 +0600

    MVC_12_first

---
+++ MVC_12/Work_01/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Work_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Score()
+        {
+            Thread.Sleep(2000);
+            return PartialView();
+        }
+
+        public JsonResult CurrentScore()
+        {
+            Thread.Sleep(2000);
+            return Json(new { over = 17.5, score="178/3" });
+        }
+    }
+}
