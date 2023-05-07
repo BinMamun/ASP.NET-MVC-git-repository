bd389ec76f088015f5bcb0724ede0bc709a0a1f0
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-07 20:17:09 +0600

    MVC_02

---
+++ MVC_02/Work_01/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work_01.Models;
+
+namespace Work_01.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly MVC_02DbContext db = new MVC_02DbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Count = db.Customers.Count();
+            ViewData["FirstCustomer"] = db.Customers.First().CompanyName;
+            return View(db.Customers.OrderBy(c=> c.CustomerID).ToList());
+        }
+    }
+}
