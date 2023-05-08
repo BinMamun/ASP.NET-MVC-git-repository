ae426485613f07dca51125633c4aa8e79083ae1b
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-09 00:20:50 +0600

    MVC_14

---
+++ MVC_14/Work_01/Controllers/EmployeesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work_01.Models;
+
+namespace Work_01.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDbContext db = new EmployeeDbContext();
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(e);
+                db.SaveChanges();
+                return PartialView("_SuccessPartial");
+            }
+            return PartialView("_FailPartial");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(db.Employees.First(x=> x.Id == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(e).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(e);
+        }
+    }
+}
---
+++ MVC_14/Work_01/Models/DbModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Work_01.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+        [Required, Column(TypeName ="date"), Display(Name ="Join Date"), DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
+        public DateTime JoinDate { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+    }
+
+    public class EmployeeDbContext: DbContext
+    {
+        public EmployeeDbContext()
+        {
+            Database.SetInitializer(new Seeder());
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Seeder: DropCreateDatabaseIfModelChanges<EmployeeDbContext>
+    {
+        protected override void Seed(EmployeeDbContext context)
+        {
+            context.Employees.Add(new Employee { Name = "E1", Salary = 451000M, JoinDate = DateTime.Now});
+            context.Employees.Add(new Employee { Name = "E2", Salary = 652100M, JoinDate = DateTime.Now});
+            context.SaveChanges();
+        }
+    }
+}
