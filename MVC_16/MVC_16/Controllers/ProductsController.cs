cf43957a6ddbdfd85a473844c8ae581c81283112
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-14 17:40:38 +0600

    MVC_16_second

---
+++ MVC_16/MVC_16/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using MVC_16.Models;
+using MVC_16.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_16.Controllers
+{
+    public class ProductsController : Controller
+    {
+        readonly StoreDbContext db = new StoreDbContext();   
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Create() 
+        {
+            ViewBag.Brand = db.Brands.Select(x => new {x.BrandId, x.BrandName}).ToList();
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult Create(ProductViewModel p)
+        {
+            if(ModelState.IsValid)
+            {
+                Product pd = new Product
+                {
+                    ProductName = p.ProductName,
+                    BrandId = p.BrandId,
+                    ReleaseDate = p.ReleaseDate,
+                    Discontinued = p.Discontinued,
+                    Price = p.Price,
+                    Picture = "images.png"
+                };
+
+                if(p.Picture != null)
+                {
+                    string fileName = Guid.NewGuid() + Path.GetExtension(p.Picture.FileName);
+                    p.Picture.SaveAs(Server.MapPath("~/Images/") + fileName);
+                    pd.Picture = fileName;
+                }
+                db.Products.Add(pd);
+                db.SaveChanges();
+            }
+            return View(p);
+        }
+    }
+}
---
+++ MVC_16/MVC_16/ViewModel/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MVC_16.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_16.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        [Required, StringLength(40), Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Required, Display(Name = "Release Date"), DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        [Required,DisplayFormat(DataFormatString = "{0: 0.00}")]
+        public decimal Price { get; set; }
+        public HttpPostedFileBase Picture { get; set; }
+        public bool Discontinued { get; set; }
+        //Foreign Key       
+        public int BrandId { get; set; }
+        //Navigation
+        public Brand Brand { get; set; }
+    }
+}
