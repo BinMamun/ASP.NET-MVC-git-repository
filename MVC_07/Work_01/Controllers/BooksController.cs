b65d58809952ca617a05caed2722e8ce43bd223b
Abdullah Bin Mamun <binbean44@gmail.com>
2023-05-21 21:09:53 +0600

    MVC_07

---
+++ MVC_07/Work_01/Controllers/BooksController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work_01.Models;
+
+namespace Work_01.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookDbContext db = new BookDbContext();
+        // GET: Books
+        public ActionResult Index()
+        {
+            ViewBag.Count = db.Books.Count();
+            return View(db.Books.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreOptions = new string[] { "Programming", "Web Authoring", "Frontend Development", "Backend Development" };
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book b)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(b);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreOptions = new string[] { "Programming", "Web Authoring", "Frontend Development", "Backend Development" };
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            return View(db.Books.First(x => x.BookId == id));
+        }
+        public ActionResult Edit(int id)
+        {
+            ViewBag.GenreOptions = new string[] { "Programming", "Web Authoring", "Frontend Development", "Backend Development" };
+            return View(db.Books.First(x=> x.BookId == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Book b)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(b).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreOptions = new string[] { "Programming", "Web Authoring", "Frontend Development", "Backend Development" };
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.Books.First(x => x.BookId == id));
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DoDelete(Book book)
+        {
+            db.Entry(book).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_07/Work_01/Models/DbModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Work_01.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [Required(ErrorMessage ="Please enter a book name"), StringLength(50)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please Select a genre"), StringLength(50)]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "Please enter price"), Column(TypeName = "money")]
+        public decimal Price{ get; set; }
+    }
+    public class BookDbContext: DbContext
+    {
+        public BookDbContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<BookDbContext>
+    {
+        protected override void Seed(BookDbContext context)
+        {
+            Book[] books = new Book[]
+            {
+                new Book{Title="MVC-05", Genre="Programming", Price=295.5M},
+                new Book{Title="C#", Genre="Programming", Price=200M},
+                new Book{Title="Angular", Genre="Programming", Price=305.05M}
+            };
+            context.Books.AddRange(books);
+        }
+    }
+}
