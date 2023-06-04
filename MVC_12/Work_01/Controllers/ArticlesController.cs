b122bc89f053efee9d697a4090d7fb773d3013b2
Abdullah Bin Mamun <binbean44@gmail.com>
2023-06-05 00:15:47 +0600

    MVC_12

---
+++ MVC_12/Work_01/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Work_01.Models;
+
+namespace Work_01.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly ArticleDbContext db = new ArticleDbContext();
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult ArticleList(int pg=1)
+        {
+            ViewBag.CurrentPage = pg;
+            ViewBag.TotalPages = (int)Math.Ceiling((db.Articles.Count() / 5.0));
+            return PartialView("_ArticleTable", db.Articles.OrderBy(x=> x.Id).Skip((pg-1)*5).Take(5).ToList());
+        }
+    }
+}
---
+++ MVC_12/Work_01/Models/DbModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Work_01.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [Required, StringLength(40)]
+        public string Title { get; set; }
+        [Required, Column(TypeName ="date"), Display(Name ="Release Date"), DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}")]
+        public DateTime ReleaseDate { get; set; }
+        [Required, Display(Name ="User Rating")]
+        public double UserRating { get; set; }
+        [Required, Display(Name ="Total View")]
+        public int TotalView { get; set; }
+    }
+
+    public class ArticleDbContext : DbContext
+    {
+        public ArticleDbContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+        public DbSet<Article> Articles { get; set; }
+    }
+
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<ArticleDbContext>
+    {
+        protected override void Seed(ArticleDbContext context)
+        {
+            context.Articles.Add(new Article { Title = "Rise of Taliban", ReleaseDate = new DateTime(2016, 08, 21), UserRating = 4.5, TotalView = 1547800 });
+            context.Articles.Add(new Article { Title = "The world's shop", ReleaseDate = new DateTime(2018, 03, 07), UserRating = 4.7, TotalView = 185900 });
+            context.Articles.Add(new Article { Title = "Covid-19", ReleaseDate = new DateTime(2020, 02, 20), UserRating = 4.2, TotalView = 252000 });
+            context.SaveChanges();
+        }
+    }
+}
